a4f96461017bac541af047b8f2a2247f1753e6a8
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-03-11 20:53:48 +0200

    change Main component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Search } from "lucide-react";
+import Image from "next/image";
 
 const newsData = [
   {
@@ -9,7 +10,7 @@ const newsData = [
     date: "7 Березня, 2025",
     author: "Анна",
     description: "БФ 'Право на захист' проводить збір даних для участі в програмі багатоцільової грошової допомоги...",
-    image: "./img/image1.png",
+    image: "/img/image1.png", 
   },
   {
     id: 2,
@@ -17,7 +18,7 @@ const newsData = [
     date: "5 Березня, 2025",
     author: "Іван",
     description: "Останні оновлення у сфері оподаткування для малого бізнесу...",
-    image: "./img/image.png",
+    image: "/img/image.png", 
   },
   {
     id: 3,
@@ -25,7 +26,7 @@ const newsData = [
     date: "7 Березня, 2025",
     author: "Анна",
     description: "БФ 'Право на захист' проводить збір даних для участі в програмі багатоцільової грошової допомоги...",
-    image: "./img/image.png",
+    image: "/img/image.png", 
   },
 ];
 
@@ -34,7 +35,7 @@ const Main = () => {
 
   return (
     <main className="bg-[#0A0A0A] text-white min-h-screen flex flex-col items-center p-6">
-      <div className="relative flex items-center w-full max-w-md">
+      <div className="relative flex items-center w-full max-w-md mb-10">
         <input
           type="text"
           placeholder="Пошук..."
@@ -44,14 +45,22 @@ const Main = () => {
         />
         <Search className="absolute left-3 text-gray-400" size={24} />
       </div>
-
-      <div className="mt-10 w-full max-w-3xl ">
+      <div className="w-full max-w-3xl">
         <h2 className="text-2xl font-semibold text-center border-b text-white mb-4">Останні новини</h2>
-        <div className="grid gap-6">
+        <div className="flex flex-col gap-6">
           {newsData.map((news) => (
-            <div key={news.id} className=" p-4 shadow-lg flex gap-4 border-b">
-              <img src={news.image} alt={news.title} className="w-50 h-50 object-cover " />
-              <div>
+            <div
+              key={news.id}
+              className="p-4 shadow-lg flex flex-col md:flex-row gap-4 border-b"
+            >
+              <Image
+                src={news.image}
+                alt={news.title}
+                width={300}
+                height={200} 
+                className="object-cover rounded-md w-full md:w-[300px] md:h-[200px]" 
+              />
+              <div className="flex flex-col gap-2">
                 <h3 className="text-lg font-semibold">{news.title}</h3>
                 <p className="text-gray-400 text-sm">
                   {news.author} - {news.date}
